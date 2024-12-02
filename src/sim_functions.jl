b01a694bd9052ae27f90e687d4672cce61123396
ReneSkukies <ReneSkukies@gmx.de>
2024-12-02 12:25:09 +0100

    work on jitter sims

--- src/sim_functions.jl
+++ src/sim_functions.jl
@@ -1,6 +1,6 @@
 # Function to be used in simulations
 
-function jitter_simulation(d::Dict)
+function jitter_simulation(d::Dict; τ=[-0.1, 1], sfreq=100)
     @unpack noiselevel shuffle offset width seed
 
     ## Components
@@ -17,18 +17,18 @@ function jitter_simulation(d::Dict)
 
     p3 = LinearModelComponent(;
         basis=p300(),
-        formula=@formula(0 ~ 1 + continuous + continuous^2),
-        β=[5, 1, 0.2],
+        formula=@formula(0 ~ 1),
+        β=[7],
     )
-    
+
     components = [p1, n1, p3]
     ## Design
     if shuffl
         design =
             SingleSubjectDesign(;
                 conditions=Dict(
-                    :condition => ["bike", "face"],
-                    :continuous => range(0, 5, length=10),
+                    :condition => ["bike"] #:condition => ["bike", "face"],
+                    #:continuous => range(0, 5, length=10),
                 ),
                 event_order_function=x -> shuffle(MersenneTwister(seed), x),
             ) |> x -> RepeatDesign(x, 15)
@@ -36,15 +36,15 @@ function jitter_simulation(d::Dict)
         design =
             SingleSubjectDesign(;
                 conditions=Dict(
-                    :condition => ["bike", "face"],
-                    :continuous => range(0, 5, length=10),
+                    :condition => ["bike"] #:condition => ["bike", "face"],
+                    #:continuous => range(0, 5, length=10),
                 ),
             ) |> x -> RepeatDesign(x, 15)
     end
 
 
     ## Grund Truth design
-    effects_dict = Dict(:condition => ["bike", "face"])
+    effects_dict = Dict(:condition => ["bike"]) #effects_dict = Dict(:condition => ["bike", "face"])
 
     effects_design = EffectsDesign(design, effects_dict)
 
@@ -61,26 +61,38 @@ function jitter_simulation(d::Dict)
     ## simulate ground truth
 
     gt_data, gt_events = simulate(
-        MersenneTwister(1),
+        MersenneTwister(seed),
         effects_design,
         components,
-        UniformOnset(; width = 0, offset = 1000),
+        UniformOnset(; width=0, offset=1000),
         NoNoise(),
-        return_epoched = true,
-    );
+        return_epoched=true,
+    )
 
-    g = reshape(gt_data,1,size(gt_data)...)
-    times = range(1, 45);
+    g = reshape(gt_data, 1, size(gt_data)...)
+    times = range(1, 45)
     gt_effects = Unfold.result_to_table([g], [gt_events], [times], ["effects"])
-    
+
     # Fit Unfold
     m = fit(
         UnfoldModel,
         [Any => (@formula(0 ~ 1 + condition + spl(continuous, 4)),
-            firbasis(τ=[-0.1, 1], sfreq=100, name="basis"),
+            firbasis(τ=τ, sfreq=100, name="basis"),
         )],
         evts,
         data,
     )
-    return m # ground truth
+
+    result_effects = effects(effects_dict, m); 
+
+    return DataFrame(
+        results = result_effects,
+        ground_truth = gt_effects,
+        model = m,
+        noiselevel = noiselevel,
+        shuffle = shuffle,
+        offset = offset,
+        width = width,
+        seed = seed
+        )
 end
