9092d495e5d095826604e4c3cd3a308b0a8c2696
ReneSkukies <ReneSkukies@gmx.de>
2025-09-19 14:25:52 +0200

    combine jitter simulations

--- scripts/202411_jitter_simulations.jl
+++ scripts/202411_jitter_simulations.jl
@@ -3,7 +3,8 @@ using DrWatson
 @quickactivate :BreakUnfold
 
 # FRP, RT, NAT simulations
-dicts = set_up_parameters([FRP_sim, NAT_sim]; offset=[15], width=[5, 10, 20, 30, 50, 100], seed=collect(1:10), sfreq=100)
+force_simulation = false
+dicts = set_up_parameters([RT_sim, FRP_sim, NAT_sim]; shuffle=[true, false], offset=[15], width=[1, 5, 10, 20, 30, 50, 100], seed=collect(1:50), sfreq=100, ntrials=[200]) 
 
 # Simulations
 all_results = DataFrame()
@@ -11,7 +12,7 @@ all_results = DataFrame()
 pbar = ProgressBar(total=size(dicts, 1))
 @time for d in dicts
     update(pbar)
-    tmp_data, tmp_file = produce_or_load(jitter_simulation, d, datadir("jitter_simulations"); force=true, tag=false)
+    tmp_data, tmp_file = produce_or_load(jitter_simulation, d, datadir("jitter_simulations"); force=force_simulation, tag=false)
     
     if typeof(tmp_data) == Dict{String, Any}
         append!(all_results, tmp_data["df"])
