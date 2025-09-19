97d48a7533c9d661b5a27304fc66fafa78f86926
ReneSkukies <ReneSkukies@gmx.de>
2025-09-19 14:31:38 +0200

    add trial number simulations

---
+++ scripts/20250901_trial_number_simulations.jl
@@ -0,0 +1,22 @@
+# Activate DrWatson
+using DrWatson
+@quickactivate :BreakUnfold
+
+# FRP, RT, NAT simulations
+force_simulation = false
+dicts = set_up_parameters([RT_sim, FRP_sim, NAT_sim]; ntrials=[50, 100, 200, 300], offset=[15], width=[5, 45], seed=collect(1:50), sfreq=100)
+
+# Simulations
+all_results = DataFrame()
+#all_results_RT = DataFrame()
+pbar = ProgressBar(total=size(dicts, 1))
+@time for d in dicts
+    update(pbar)
+    tmp_data, tmp_file = produce_or_load(jitter_simulation, d, datadir("trial_number"); force=force_simulation, tag=false)
+    
+    if typeof(tmp_data) == Dict{String, Any}
+        append!(all_results, tmp_data["df"])
+    else
+        append!(all_results, tmp_data)
+    end 
+end
